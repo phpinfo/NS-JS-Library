9bccc355f989f8e0c5cb20f7dd4f2ece0e31cb87
Alexey <italianwind@gmail.com>
2010-04-05 14:19:12 +0400

    Placeholder updated

--- placeholder.js
+++ placeholder.js
@@ -1,53 +1,136 @@
-if (typeof(NS) == 'undefined') NS = {};
+/*
+---
+description: This simple plugin provides HTML 5 placeholder attribute to all browsers.
 
-/**
- * NS Placeholder
- * 
- */
-$(window).addEvent('domready', function(){
-	// Защита от браузеров, уже поддерживающих этот атрибут
-	if ('placeholder' in document.createElement('input')) return;
+license: MIT-style
 
-	// Цвет
-	var color = '#aaa';
+authors:
+- Alexey Gromov
 
-	// Обработка всех полей ввода
-	$$('input').each(function(el){
+requires:
+  core/1.2.4: '*'
 
-		// Текст для placeholder'a
-		var text = el.getAttribute('placeholder');
-		// Цвет текста по умолчанию
-		var defaultColor = el.getStyle('color');
+provides:
+  NS.Placeholder
 
-		if (text)
+...
+*/
+var NS = NS || {};
+NS.Placeholder = new Class({
+	Implements: [Options],
+
+	options: {
+		/**
+		 * Elements to check
+		 * (single element, elements collection or a string selector)
+		 * @var {Element|Elements|String}
+		 */
+		elements: 'input[type=text]',
+
+		/**
+		 * CSS class when value is empty
+		 * @var {String}
+		 */
+		cssClass: 'placeholder',
+
+		/**
+		 * Color to apply directly
+		 * @var {String}
+		 */
+		color: null
+	},
+
+	/**
+	 * Initialization
+	 *
+	 * @param {Object}
+	 */
+	initialize: function(options)
+	{
+		// Setting options
+		this.setOptions(options);
+
+		// Retrieving elements to check
+		var elements;
+		switch ($type(this.options.elements))
 		{
-			// Установка начальных значений
-			el.setStyle('color', color);
-			el.value = text;
-
-			// Обработчики событий
-			el.addEvent('focus', function(){
-				if (this.value == '' || this.value == text)
-				{
-					this.setStyle('color', defaultColor);
-					this.value = '';
-				}
-			});
-			el.addEvent('blur', function(){
-				if (this.value == '' || this.value == text)
-				{
-					el.setStyle('color', color);
-					el.value = text;
-				}
-			});
-			var f = el.getParents('form');
-			if (f.length)
-			{
-				f[0].addEvent('submit', function(){
-					if (el.value == text)
-						el.value = '';
-				});
-			}
+			case 'string':
+				elements = $$(this.options.elements);
+				break;
+			case 'element':
+				elements = [this.options.elements];
+				break;
+			default:
+				elements = this.options.elements;
 		}
-	});
+
+		// Attaching events
+		elements.each(function(el){
+			var text = el.get('placeholder');
+			if (text)
+			{
+				// Storing placeholder text
+				el.store('ns-placeholder-text', text);
+
+				// Storing default color
+				el.store('ns-placeholder-color', el.getStyle('color'));
+
+				// Bluring
+				this.blur(el);
+
+				// Events
+				el.addEvents({
+					focus: function(){ this.focus(el); }.bind(this),
+					blur: function(){ this.blur(el); }.bind(this)
+				});
+
+				// Form submit
+				var form = el.getParent('form');
+				if (form)
+				{
+					form.addEvent('submit', function()
+					{
+						if (el.value == text)
+							el.set('value', '');
+					});
+				}
+			}
+		}.bind(this));
+	},
+
+	/**
+	 * Focus element
+	 * @param {Element}
+	 * @param {Boolean}
+	 */
+	focus: function(el, focus)
+	{
+		focus = focus == undefined || focus;
+
+		var text = el.retrieve('ns-placeholder-text'),
+			value = el.get('value');
+
+		if (value == '' || value == text)
+		{
+			// Setting placeholder CSS class if defined
+			if (this.options.cssClass)
+				el[focus ? 'removeClass' : 'addClass'](this.options.cssClass);
+
+			// Setting color if defined
+			if (this.options.color)
+				el.setStyle('color', focus ? el.retrieve('ns-placeholder-color') : this.options.color);
+
+			// Value
+			el.set('value', focus ? '' : text);
+		}
+	},
+
+	/**
+	 * Blur element
+	 * @param {Element}
+	 */
+	blur: function(el)
+	{
+		this.focus(el, false);
+	}
 });
